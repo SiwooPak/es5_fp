a8d0811966ce0d83563379484cc90b67f7224439
SiwooPak <bogus1620@naver.com>
2022-01-11 04:55:32 +0900

    modify ch02.js

--- ch02.js
+++ ch02.js
@@ -61,3 +61,48 @@ console.log(get_name(user1));
 
 const user2 = { id: 2, name: "BJ", age: 32 };
 console.log(get_name(user2));
+
+// 기존 코드 최적화
+const users = [
+  { id: 1, name: "ID", age: 36 },
+  { id: 2, name: "BJ", age: 32 },
+  { id: 3, name: "JM", age: 32 },
+  { id: 4, name: "PJ", age: 27 },
+  { id: 5, name: "HA", age: 25 },
+  { id: 6, name: "JE", age: 26 },
+  { id: 7, name: "JI", age: 31 },
+  { id: 8, name: "MP", age: 23 },
+];
+
+const _filter = (lists, predi) => {
+  const new_list = [];
+  lists.forEach((el) => {
+    if (predi(el)) new_list.push(el);
+  });
+  return new_list;
+};
+
+const _each = (list, iter) => {
+  list.forEach((el) => iter(el));
+  return list;
+};
+
+const _map = (lists, mapper) => {
+  const new_list = [];
+  _each(lists, (val) => {
+    new_list.push(mapper(val));
+  });
+  return new_list;
+};
+
+const result = _map(
+  _filter(users, (user) => user.age < 30),
+  _get2("name")
+);
+console.log("result: " + result);
+
+const result2 = _map(
+  _filter(users, (user) => user.age >= 30),
+  _get2("age")
+);
+console.log("result2: " + result2);
